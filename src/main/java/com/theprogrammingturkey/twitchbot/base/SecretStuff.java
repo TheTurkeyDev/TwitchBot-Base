556ee2c57e66a6667110edb4be5050dadbeb134f
Turkey2349 <turkey2349@gmail.com>
2019-10-04 00:09:50 -0400

    Added a secretstuff file

---
+++ src/main/java/com/theprogrammingturkey/twitchbot/base/SecretStuff.java
@@ -0,0 +1,8 @@
+package com.theprogrammingturkey.twitchbot.base;
+
+public class SecretStuff
+{
+	public static String botName = "";
+	public static String oAuth = "";
+	public static String clientID ="";
+}
